963fb1ada99c9a2e9d8619c1c52b5bfdb8432f63
Entony <franciscoentonydev@gmail.com>
2026-06-03 00:49:21 -0300

    Build: configurando deploy automático do client via github actions

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,10 +3,8 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from "@tailwindcss/vite"
 
 // https://vite.dev/config/
-export default defineConfig(({ command }) => {
-  return {
-    plugins: [react(), tailwindcss()],
-    base: command === 'build' ? '/site-sei/' : '/',
-  }
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  base: '/site-sei/', // 👈 Adicione essa linha com o nome do seu repositório
 })
 
